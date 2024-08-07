5b7c58b83678261f1851bad4ca86afae5f7674e3
orange2458 <asm1133@naver.com>
2024-08-07 14:24:37 +0900

    동일한 자원에 대한 접근을 동기화 해주는 작업 - Lock 기초

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -3,33 +3,66 @@
 #include "CorePch.h"
 #include <thread>
 #include <atomic>
+#include <mutex>
 
-// atomic : All-Or-Noting
-atomic<int32> sum = 0;
+vector<int32> v;
 
-void Add( )
+// Mutual Exlusive (상호배타적)
+mutex m;
+
+// RAII (Resource Acquisition Is Initialization)
+// mutex의 lock, uulock을 생성자, 소멸자에서 처리하면 편리하다.
+template<class T>
+class LockGuard		// 표준에 std::lock_guard가 있음. LockGuard는 느낌을 파악하기 위함.
 {
-	for ( int32 i = 0; i < 100'0000; ++i ) {
-		sum.fetch_add( 1 );
+public:
+	LockGuard( T& mutex )
+		: mutex_( &mutex )
+	{
+		mutex_->lock( );
 	}
-}
 
-void Sub( )
+	~LockGuard( )
+	{
+		mutex_->unlock( );
+	}
+
+private:
+	T* mutex_;
+};
+
+void Push( )
 {
-	for ( int32 i = 0; i < 100'0000; ++i ) {
-		sum.fetch_add( -1 );
+	for ( int32 i = 0; i < 10000; ++i ) {
+		// 자물쇠 잠그기
+		LockGuard<std::mutex> lockGuard{ m };	// std::lock_guard<std::mutex> lockGuard{ m }; 와 같음.
+
+		// std::unique_lock<std::mutex> uniqueLock{ m, std::defer_lock };
+		// defer_lock : lock을 하지 않고 생성만 함. 나중에 lock을 하기 위함.
+		// uniqueLock.lock( );	// lock을 하고 싶을 때 사용.
+		// 소멸자에서 unlock을 하기 때문에 unlock을 하지 않아도 됨.
+
+		//m.lock( );
+		//m.lock( );	// 재귀 lock 허용하는 경우도 있음.
+
+		v.push_back( i );
+
+		if ( i == 5000 )
+			break;
+
+		// 자물쇠 풀기
+		//m.unlock( );
+		//m.unlock( );
 	}
 }
 
 int main()
 {
-	Add( );
-	Sub( );
-	cout << sum << endl;
+	std::thread t1{ Push };
+	std::thread t2{ Push };
 
-	std::thread t1( Add );
-	std::thread t2( Sub );
 	t1.join( );
 	t2.join( );
-	cout << sum << endl;
+
+	cout << v.size( ) << endl;
 }
