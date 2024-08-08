a2dec1fa5978dd0ff636df4994540f7202632f4d
orange2458 <asm1133@naver.com>
2024-08-08 23:15:24 +0900

    Event(커널 오브젝트)를 이용한 스레드 동기화

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -4,62 +4,56 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <Windows.h>
 
-class SpinLock
-{
-public:
-	void lock( )
-	{
-		// CAS (Compare-And-Swap)
-		
-		bool expected = false;
-		bool desired = true;
-
-		while ( locked_.compare_exchange_strong( expected, desired ) == false ) {
-			expected = false;
-
-			//this_thread::sleep_for( std::chrono::milliseconds( 100 ) );	// Or this_thread::sleep_for( 100ms );
-			this_thread::sleep_for( 0ms );
-			//this_thread::yield( );
-		}
-	}
-
-	void unlock( )
-	{
-		locked_.store( false );
-	}
-
-private:
-	atomic<bool> locked_ = false;
-};
-
-int32 sum = 0;
 mutex m;
-SpinLock spinLock;
+queue<int32> q;
+HANDLE handle;
 
-void Add( )
+void Producer( )
 {
-	for ( int32 i = 0; i < 10'0000; ++i ) {
-		lock_guard<SpinLock> guard{ spinLock };
-		++sum;
+	while ( true ) {
+		{
+			unique_lock<mutex> lock{ m };
+			q.push( 100 );
+		}
+
+		::SetEvent( handle );	// 커널 오브젝트의 Signal 상태를 true로 변경
+
+		this_thread::sleep_for( 1000ms );
 	}
 }
 
-void Sub( )
+void Consumer( )
 {
-	for ( int32 i = 0; i < 10'0000; ++i ) {
-		lock_guard<SpinLock> guard{ spinLock };
-		--sum;
+	while ( true ) {
+		::WaitForSingleObject( handle, INFINITE );	// 커널 오브젝트가 Signal(true) 상태면 진행, Non-Signal(false) 상태면 스레드가 잠듦. CPU 점유율을 낮추기 위해 사용
+		// 다시 이 스레드가 실행이 되면, CreateEvent 함수의 bManualReset이 false(Auto)기 때문에
+		// Signal 상태가 자동으로 Non-Signal 상태로 변경됨.
+
+		unique_lock<mutex> lock{ m };
+		
+		if ( q.empty( ) == false ) {
+			int32 data = q.front( );
+			q.pop( );
+			cout << data << endl;
+		}
 	}
 }
 
 int main()
 {
-	thread t1{ Add };
-	thread t2{ Sub };
+	// 커널 오브젝트
+	// Usage Count : 몇 개의 스레드가 이 오브젝트를 사용하고 있는지
+	// Signal (파란불) / Non-Signal (빨간불) << bool
+	// Auto / Manual << bool
+	handle = ::CreateEvent( nullptr/*보안속성*/, false, false/*true : Signal, false : Non-Signal*/, nullptr );
+	
+	thread producer{ Producer };
+	thread consumer{ Consumer };
 
-	t1.join( );
-	t2.join( );
+	producer.join( );
+	consumer.join( );
 
-	cout << sum << endl;
+	::CloseHandle( handle );
 }
