bc3abf0b3e8716f97c0bf2b7c5325faaa5506aa5
orange2458 <asm1133@naver.com>
2024-08-10 02:55:01 +0900

    메모리 모델

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -7,61 +7,68 @@
 #include <future>
 #include <Windows.h>
 
-// 가시성)
-// CPU의 각 코어마다 자신만의 캐시를 가지고 있다.
-// 캐시에는 메모리의 일부분을 복사해 놓는다.
-// 캐시에 복사된 데이터를 수정하면 메모리에 있는 데이터와 일치하지 않는다.
-// Thread_1에서 y = 1이 실행된다 하더라도, Thread_2에서 r2 = y가 실행될 때의 y의 값이 1이 아닐 수 도 있다는 얘기다.
-// 
-// 코드 재배치)
-// 컴파일러나 CPU는 코드를 재배치할 수 있다.
+atomic<bool> ready;
+int32 value;
+
+void Producer( )
+{
+	value = 10;
+
+	ready.store( true, memory_order::memory_order_seq_cst );
+}
+
+void Consumer( )
+{
+	while ( ready.load( memory_order::memory_order_seq_cst ) == false );
+
+	cout << value << endl;
+}
+
+//void Producer( )
+//{
+//	value = 10;
 //
-// 따라서 if ( r1 == 0 && r2 == 0 ) break; 이 조건문이 참이 되는 경우가 발생할 수 있다.
-
-int32 x;
-int32 y;
-int32 r1;
-int32 r2;
-
-volatile bool ready;
-
-void Thread_1( )
-{
-	while ( !ready );
-
-	y = 1; // Store y
-	r1 = x; // Load x
-}
-
-void Thread_2( )
-{
-	while ( !ready );
-
-	x = 1; // Store x
-	r2 = y; // Load y
-}
+//	ready.store( true, memory_order::memory_order_release );
+//	// ----------------------절취선------------------------- 위의 코드가 이 선 아래로 재배치 금지
+//}
+//
+//void Consumer( )
+//{
+//	// ----------------------절취선------------------------- 아래의 코드가 이 선 위로 재배치 금지
+//	while ( ready.load( memory_order::memory_order_acquire ) == false );
+//
+//	cout << value << endl;
+//}
 
 int main( )
 {
-	int32 count = 0;
+	ready = false;
+	value = 0;
+	thread t1{ Producer };
+	thread t2{ Consumer };
+	t1.join( );
+	t2.join( );
 
-	while ( true ) {
-		ready = false;
-		count++;
+	// Memory Model (정책)
+	// 1) Sequentially Consistent (순차적 일관성) / seq_cst
+	// 2) Acquire-Release (획득-방출) / acquire, release
+	// 3) Relaxed (완화) / relaxed
 
-		x = y = r1 = r2 = 0;
+	// 1) seq_cst (가장 엄격 = 컴파일러 최적화 여지 적음 = 직관적)
+	//		- 가시성 문제 바로 해결! 코드 재배치 바로 해결!
 
-		thread t1{ Thread_1 };
-		thread t2{ Thread_2 };
+	// 2) acquire-release
+	//		- 딱 중간!
+	//		- release 명령 이전의 메모리 명령들이 해당 명령 이후로 재배치되는 것을 금지
+	//		- 그리고 acquire로 같은 변수를 읽는 쓰레드가 있다면,
+	//		  release 이전의 명령들이 -> acquire 하는 순간에 관찰 가능 (가시성 보장)
 
-		ready = true;
+	// 3) relaxed (자유롭다 = 컴파일러 최적화 여지 많음 = 직관적이지 않음)
+	//		- 너무나도 자유롭다!
+	//		- 코드 재배치도 멋대로 가능! 가시성 해결 NO!
+	//		- 가장 기본 조건 (동일 객체에 대한 동일 관전 순서만 보장)
 
-		t1.join( );
-		t2.join( );
-
-		if ( r1 == 0 && r2 == 0 )
-			break;
-	}
-
-	cout << count << " 번만에 빠져나옴" << endl;
+	// 인텔, AMD의 경우 애당초 순차적 일관성을 보장을 해서,
+	// seq_cst를 써도 별다른 부하가 없음
+	// ARM의 경우 꽤 차이가 있다!
 }
